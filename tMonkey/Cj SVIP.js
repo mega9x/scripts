c283d01bbcb29d36abd874ca4aa6afb30d91ba65
Omega <megaTechart@outlook.com>
2022-12-08 21:50:53 +0800

    update: cj getlinks fix 1

--- tMonkey/Cj SVIP.js
+++ tMonkey/Cj SVIP.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Omega
-// @match        https://members.cj.com/member/5849095/publisher/links/search/*
+// @match        https://members.cj.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cj.com
 // @grant        none
 // ==/UserScript==
